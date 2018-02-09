4add1509f65ce36a52db44f4ab45ac5e2691f33c
Ethan Green <greenmarleyethan@gmail.com>
2018-02-09 00:52:19 -0500

    Finished chapter 05 exercise

--- CSC134/Chap05/Ch5Intro14Project/Ch5Intro14.cpp
+++ CSC134/Chap05/Ch5Intro14Project/Ch5Intro14.cpp
@@ -8,11 +8,39 @@ using namespace std;
 
 int main()
 {
+  int patientAge = 0;
 	float baseLevelBMR = 0.0;
+	float patientHeightFeet = 0.0;
+	float patientWeightLb = 0.0;
 	char patientSex = ' ';
 
-	cout >> "Is your patient male or female? (M/F): ";
-	cin << tolower << patientSex;
-
+	cout << "Is your patient male or female? (M/F): ";
+	cin >> patientSex;
+	
+	patientSex = tolower(patientSex);
+	
+	cout << "How old is the patient? (Years): ";
+	cin >> patientAge;
+	
+	cout << "What is the height of the patient? (Feet): ";
+	cin >> patientHeightFeet;
+	
+	cout << "What is the weight of the patient? (LB): ";
+	cin >> patientWeightLb;
+  
+  baseLevelBMR = (10 * (patientWeightLb / 2.2)) + (6.25 * ((patientHeightFeet * 12) * 2.54)) - (5 * patientAge);
+  
+  if (patientSex == 'M')
+  {
+    baseLevelBMR += 5;
+  }
+  
+  else if (patientSex == 'F')
+  {
+    baseLevelBMR -= 161;
+  }
+  
+  cout << "The patients BMR is: " << baseLevelBMR;
+  
 	
 }
